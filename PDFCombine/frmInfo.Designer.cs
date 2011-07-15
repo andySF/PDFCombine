f776be1f6ce9c58df466ba8574ed28ed8ba450dd
andySF <andySF@gmail.com>
2011-07-15 19:59:56 +0300

    info is ready

--- PDFCombine/frmInfo.Designer.cs
+++ PDFCombine/frmInfo.Designer.cs
@@ -29,30 +29,47 @@
         private void InitializeComponent()
         {
             this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.btnOk = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBoxInfo
             // 
+            this.richTextBoxInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBoxInfo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBoxInfo.Location = new System.Drawing.Point(10, 10);
             this.richTextBoxInfo.Name = "richTextBoxInfo";
-            this.richTextBoxInfo.Size = new System.Drawing.Size(463, 303);
+            this.richTextBoxInfo.ReadOnly = true;
+            this.richTextBoxInfo.Size = new System.Drawing.Size(433, 398);
             this.richTextBoxInfo.TabIndex = 0;
             this.richTextBoxInfo.Text = "";
+            this.richTextBoxInfo.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBoxInfo_LinkClicked);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(10, 408);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(433, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Nice, Thanks!";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // frmInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 323);
+            this.ClientSize = new System.Drawing.Size(453, 441);
             this.Controls.Add(this.richTextBoxInfo);
+            this.Controls.Add(this.btnOk);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmInfo";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Information";
+            this.Text = "Usefull informations";
             this.ResumeLayout(false);
 
         }
@@ -60,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.Button btnOk;
     }
 }
--- PDFCombine/frmInfo.cs
+++ PDFCombine/frmInfo.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
 
 namespace PDFCombine
 {
@@ -13,12 +15,32 @@ namespace PDFCombine
         public frmInfo()
         {
             InitializeComponent();
-            richTextBoxInfo.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0\deflang1033{\fonttbl{\f0\fnil\fcharset238 Calibri;}{\f1\fnil\fcharset0 Calibri;}{\f2\fnil\fcharset2 Symbol;}}
-{\*\generator Msftedit 5.41.21.2509;}\viewkind4\uc1\pard\sa200\sl276\slmult1\lang1048\b\f0\fs22 PDFCombine\par
-\pard{\pntext\f2\'B7\tab}{\*\pn\pnlvlblt\pnf2\pnindent0{\pntxtb\'B7}}\fi-360\li720\sa200\sl276\slmult1\b0 Open images\lang1033\f1\par
-\lang1048\f0{\pntext\f2\'B7\tab}Open PDF's\lang1033\f1\par
-}
-";
+     
+
+
+            try
+            {
+                Assembly creditAssm = Assembly.GetExecutingAssembly();
+                using (Stream creditStream = creditAssm.GetManifestResourceStream("PDFCombine.info.rtf"))
+                {
+                    richTextBoxInfo.LoadFile(creditStream, RichTextBoxStreamType.RichText);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error accessing resources!");
+            }
+
+        }
+
+        private void richTextBoxInfo_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
